25d61f27a0af80e8f6044c80fa20ad4b81e3129b
agajdosi <andreas.gajdosik@gmail.com>
2020-02-12 01:44:31 +0100

    Add quotes to profiles

--- profile.py
+++ profile.py
@@ -3,9 +3,14 @@ from tests import getBigFive, getSCL90
 
 def getProfile(personID):
     conn = sqlite3.connect('prod.db')
-    cursor = conn.execute("SELECT name, active, image FROM persons WHERE id = '{0}'".format(personID))
+    cursor = conn.execute("SELECT name, active, image, quote FROM persons WHERE id = '{0}'".format(personID))
     row = cursor.fetchone()
-    profile = {"id":personID, "name":row[0], "active":row[1], "image":row[2], "hlaska": "Uvažoval jsem tak, že co není zákonem zakázáno, je možné dělat."}
+    quote = row[3]
+    if quote != None:
+        quote = "„" + row[3] + "“"
+    else:
+        quote = ""
+    profile = {"id":personID, "name":row[0], "active":row[1], "image":row[2], "quote": quote}
 
     return profile
 
