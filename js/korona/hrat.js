b6a6fc3f8340dfdbff4228e85db39cbc12b0b201
agajdosi <andreas.gajdosik@gmail.com>
2020-05-04 00:16:02 +0200

    We need only one error message now!

--- js/korona/hrat.js
+++ js/korona/hrat.js
@@ -7,16 +7,8 @@ function openWS(token){
     ws = new WebSocket(url);
 
     ws.onmessage = function (evt) {
-        if (evt.data == "game is full") {
-            alert("Sorry this room is full");
-            window.location = "../";
-        };
         if (evt.data == "roles are taken") {
             alert("Sorry all roles were taken");
-            window.location = "../";
-        };
-        if (evt.data == "already connected") {
-            alert("Sorry you are already connected");
             window.location = "../";
         };
     };
