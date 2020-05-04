352c57743e71a5c8754cf244b53814a8b66f5464
agajdosi <andreas.gajdosik@gmail.com>
2020-05-04 17:33:57 +0200

    Init game parsing in JS

--- js/korona/hra.js
+++ js/korona/hra.js
@@ -1,5 +1,7 @@
+identity = getCookie("nejvyzkum-player")
 token = null
 ws = null
+
 
 function openWS(token){
     token = token
@@ -10,9 +12,41 @@ function openWS(token){
         if (evt.data == "roles are taken") {
             alert("Sorry all roles were taken");
             window.location = "../";
+            return
         };
+
+        parseGameData(evt);
+
     };
+}
+
+function parseGameData(evt){
+    game = JSON.parse(evt.data);
+    console.log(game)
+    
+    if (identity == game["witness"]) {
+        document.getElementById("role").innerText = "You are a Witness!"
+    }
+    if (identity == game["detective"]) {
+        document.getElementById("role").innerText = "You are a Detective!"
+    }
 }
 
 
 
+
+
+function endTurn() {
+    console.log("it has been sent");
+}
+
+function getCookie(name) {
+    var cookieArr = document.cookie.split(";");
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        if(name == cookiePair[0].trim()) {
+            return cookiePair[1];
+        }
+    }
+    return null;
+}
