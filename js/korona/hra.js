dc4cf09d6fd740e55f0587aa35ea814e258a4f49
agajdosi <andreas.gajdosik@gmail.com>
2020-05-13 01:12:07 +0200

    Generate 3 random hints for each round

--- js/korona/hra.js
+++ js/korona/hra.js
@@ -37,11 +37,67 @@ function parseGameData(evt){
     renderGameWon();
 }
 
+var fields = [
+    ["quote", "citát"],
+    ["mainsource", "zdroj financí"],
+    ["sidesource", "vedlejší zdroj financí"],
+    ["birthday", "narození"],
+    ["residence", "bydliště"],
+    ["birthplace", "místo narození"],
+    ["education", "vzdělání"],
+    ["maritalstatus", "rodinný stav"],
+    ["children", "děti"],
+    ["obsession", "posedlost"],
+    ["car", "auto"],
+    ["maincompany", "firma"],
+    ["sidecompanies", "vedlejší firmy"],
+    ["wealthorigin", "původ majetku"],
+    ["wealth", "jmění"],
+];
+
+var fieldsEN = [
+    ["quote-en", "quote"],
+    ["mainsource-en", "financial source"]
+    ["sidesource-en", "side fin. sources"],
+    ["birthday", "birthday"],
+    ["residence-en", "residence"],
+    ["birthplace-en", "birthplace"],
+    ["education-en", "education"],
+    ["maritalstatus-en", "marital status"],
+    ["children", "children"],
+    ["obsession-en", "obsession"],
+    ["car", "car"],
+    ["maincompany-en", "main company"],
+    ["sidecompanies-en", "side companies"],
+    ["wealthorigin-en", "wealth origin"],
+    ["wealth-en", "total wealth"],
+];
+
 function renderSuspects() {
     for(var i = 0; i < game["suspects"].length; i++) {
         document.getElementById(i).getElementsByClassName("portrait")[0].src = game["suspects"][i]["image"];
         document.getElementById(i).getElementsByClassName("criminal")[0].innerText = null;
         document.getElementById(i).getElementsByClassName("eliminated")[0].style.display = "none";
+
+        document.getElementById(i).getElementsByClassName("name")[0].innerText = game["suspects"][i]["name"];
+
+        a = game["hints"][0]
+        key1 = fields[a][0]
+        caption1 = fields[a][1]
+        text1 = caption1 + ": " + game["suspects"][i][key1]
+        document.getElementById(i).getElementsByClassName("info1")[0].innerText = text1;
+
+        b = game["hints"][1]
+        key2 = fields[b][0]
+        caption2 = fields[b][1]
+        text2 = caption2 + ": " + game["suspects"][i][key2]
+        document.getElementById(i).getElementsByClassName("info2")[0].innerText = text2;
+
+        c = game["hints"][2]
+        key3 = fields[c][0]
+        caption3 = fields[c][1]
+        text3 = caption3 + ": " + game["suspects"][i][key3]
+        document.getElementById(i).getElementsByClassName("info3")[0].innerText = text3;
     }
 
     for (var i = 0; i < game["eliminated"].length; i++) {
