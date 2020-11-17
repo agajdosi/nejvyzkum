a2f7aab2e02162d5068551def91cd4d4b9787f3e
agajdosi <andreas.gajdosik@gmail.com>
2020-11-17 20:17:12 +0100

    add redirect

--- komise.py
+++ komise.py
@@ -16,6 +16,7 @@ class Komise(general.GeneralHandler):
         text = "Uhlí smrdí a je špatné pro naše zdraví i klima, zastavme ho teď!",
         title = "Za čisté klima!",
         image = "https://cdn.xsd.cz/original/8180c83e7c94318c9a77555098e03281.jpg",
+        redirect = "https://www.facebook.com/uhelnakomise",
     )
 
   def fake(self):
@@ -24,4 +25,5 @@ class Komise(general.GeneralHandler):
       text = "Šok! Nahé fotky předsedkyně TOP09 Markéty Pekarové Adamové unikly na veřejnost! Koukněte se zde.",
       title = "Podívejte se na nahé fotky Markéty Pekarové Adamové!",
       image = "https://static.novydenik.com/2020/05/5R0A3828_1.jpg",
+      redirect = None,
     )
